c222f4b18e1ea3eda1f87bf54b2b3543605ff9a4
TolBabax <kuz.cerg@gmail.com>
2023-01-29 21:52:26 +0400

    программа, определяющая сумму цифр в числе

---
+++ task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int IntupNumber(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+
+        System.Console.WriteLine("Введено не корректное число. Попробуйте еще раз.");
+    }
+    return number;
+}
+
+int Sum(string array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += Convert.ToInt32(Convert.ToString(array[i]));
+    }
+    return sum;
+}
+void PrintArray(string array)
+{
+    int count = array.Length;
+    System.Console.Write("Элементы массива: ");
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int A = IntupNumber("Введите число A: ");
+string numberString = Convert.ToString(A);
+int sum = Sum(numberString);
+PrintArray(numberString);
+System.Console.WriteLine($"Сумма элементов массива: {sum}");
